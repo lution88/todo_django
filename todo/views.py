a48965c2bdee6a0ec609ec4881dd9eebcc52b0f4
lution88 <lution88@gmail.com>
2022-07-09 02:48:03 +0900

    add todo_post view, url

--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from todo.forms import TodoForm
 from todo.models import Todo
 
 
 def todo_list(request):
     todo = Todo.objects.filter(complete=False)
-    return render(request, 'todo_list.html', {'todo':todo})
+    return render(request, 'todo/todo_list.html', {'todo':todo})
 
 
 def todo_detail(request, pk):
     todo = Todo.objects.get(id=pk)
-    return render(request, 'todo_detail.html', {'todo':todo})
+    return render(request, 'todo/todo_detail.html', {'todo':todo})
 
 
+def todo_post(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    form = TodoForm()
+    return render(request, 'todo/todo_post.html', {'form': form})
