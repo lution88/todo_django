84a164d2058cda682585d78cdd976a67696ed508
lution88 <lution88@gmail.com>
2022-07-09 02:34:52 +0900

    add todo_detail.html

--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,8 @@ from todo.models import Todo
 def todo_list(request):
     todo = Todo.objects.filter(complete=False)
     return render(request, 'todo_list.html', {'todo':todo})
+
+
+def todo_detail(request, pk):
+    todo = Todo.objects.get(id=pk)
+    return render(request, 'todo_')
