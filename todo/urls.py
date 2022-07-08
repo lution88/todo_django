bf6a56761fc43273d42293a07b15673e090933d7
lution88 <lution88@gmail.com>
2022-07-09 02:48:54 +0900

    update todo_post url

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from todo.views import todo_list, todo_detail
+from todo.views import todo_list, todo_detail, todo_post
 
 urlpatterns = [
     path('', todo_list, name='todo_list'),
     path('<int:pk>/', todo_detail, name='todo_detail'),
-
+    path('post/', todo_post, name='todo_post'),
 ]
