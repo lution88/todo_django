83a74e61455e9fbe0712c114c4c9c4f4f47db290
lution88 <lution88@gmail.com>
2022-07-09 02:11:26 +0900

    add todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField("제목", max_length=100)
+    description = models.TextField("내용")
+    dt_created = models.DateTimeField("생성일", auto_now_add=True)
+    complete = models.BooleanField("완료 여부", default=False)
+    important = models.BooleanField("중요도", default=False)
+
+    def __str__(self):
+        return self.title
