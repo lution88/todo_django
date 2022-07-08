acea9e813a28275d4ba122e2f58a6e6a2c0afffc
lution88 <lution88@gmail.com>
2022-07-09 02:58:04 +0900

    add todo_done.html

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from todo.views import todo_list, todo_detail, todo_post
+from todo.views import todo_list, todo_detail, todo_post, todo_edit
 
 urlpatterns = [
     path('', todo_list, name='todo_list'),
     path('<int:pk>/', todo_detail, name='todo_detail'),
     path('post/', todo_post, name='todo_post'),
-    path('<int:pk>/edit/', todo_post, name='todo_post'),
+    path('<int:pk>/edit/', todo_edit, name='todo_edit'),
 ]
